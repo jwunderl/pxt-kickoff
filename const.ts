67d7c2946c29472b5be10ce30cc00770d2413799
Joey Wunderlich <jwunderl@users.noreply.github.com>
2019-09-18 11:09:46 -0700

    linting

--- const.ts
+++ const.ts
@@ -2,6 +2,8 @@ namespace SpriteKind {
     export const Ball = SpriteKind.create();
     export const Shadow = SpriteKind.create();
     export const Shield = SpriteKind.create();
+    export const PlayerTeam = SpriteKind.create();
+    export const OpposingTeam = SpriteKind.create();
 }
 
 namespace DataKeys {
--- player.ts
+++ player.ts
@@ -1,10 +1,28 @@
 namespace player {
     export function create(team: Team) {
         const startAnim = team.animations[1];
-        const player = sprites.create(startAnim.frames[0], SpriteKind.Player);
-        animation.attachAnimation(player, team.animations[AnimationDirection.Left]);
-        animation.attachAnimation(player, team.animations[AnimationDirection.Right]);
-        animation.setAction(player, team.isPlayerControlled() ? AnimationDirection.Right : AnimationDirection.Left)
+        const player = sprites.create(
+            startAnim.frames[0],
+            team.isPlayerControlled() ?
+                SpriteKind.PlayerTeam
+                :
+                SpriteKind.OpposingTeam
+        );
+        animation.attachAnimation(
+            player,
+            team.animations[AnimationDirection.Left]
+        );
+        animation.attachAnimation(
+            player,
+            team.animations[AnimationDirection.Right]
+        );
+        animation.setAction(
+            player,
+            team.isPlayerControlled() ?
+                AnimationDirection.Right
+                :
+                AnimationDirection.Left
+        );
         player.z = 2;
 
         return player;
