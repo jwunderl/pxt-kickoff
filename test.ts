8d3f7d86d2574f0755d2f572affa539bbf1a2f98
Joey Wunderlich <jwunderl@users.noreply.github.com>
2019-09-25 14:01:24 -0700

    use football.createGame in tests

--- test.ts
+++ test.ts
@@ -1,17 +1,7 @@
+// football.createGame(league.clevelandBrowns, league.seattleSeahawks);
+// football.createGame(league.kansasCityChiefs, league.pittsburghSteelers);
+football.createGame(league.newEnglandPatriots, league.washingtonRedskins);
 
-// currentGame = new Game(
-//     teams.create(league.clevelandBrowns, true),
-//     teams.create(league.seattleSeahawks, false),
-//     20
-// );
-// currentGame = new Game(
-//     teams.create(league.kansasCityChiefs, true),
-//     teams.create(league.pittsburghSteelers, false),
-//     20
-// );
-
-
-football.createGame(league.clevelandBrowns, league.seattleSeahawks)
 // controller.A.onEvent(ControllerButtonEvent.Pressed, () => ball.toss());
 
 ball.toss();
